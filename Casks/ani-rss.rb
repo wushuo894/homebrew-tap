3b034db385d7a19348ae2080e03caf933372dd5c
wushuo <wushuo894@outlook.com>
2026-06-06 16:53:44 +0000

    自动更新: 2026-06-06 16:53:44

--- Casks/ani-rss.rb
+++ Casks/ani-rss.rb
@@ -1,8 +1,8 @@
 cask "ani-rss" do
-  version "3.1.46"
+  version "3.1.47"
 
   url "https://github.com/wushuo894/ani-rss/releases/download/v#{version}/ani-rss.dmg"
-  sha256 "1f3890aa94d21fead22d7acbc669b7099cc1949b4cc16e5e8055edaf202a3051"
+  sha256 "f762e84f32e43b0800e5b4daa7419a11f8f1ab9c49b85d5b23713fa4bd377837"
 
   name "ani-rss"
   desc "基于RSS自动追番、订阅、下载、刮削、洗版"
